aac1aea4ef7ac2db8878a9100044c4633580fe04
Vinicius-ricardo <vinicius.ricardo@xpert.com.br>
2025-09-03 16:52:50 -0300

    V1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './components/todo/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [TodoComponent],      
+  template: `<app-todo></app-todo>`,
+  styleUrls: ['./app.scss']       
 })
 export class App {
   protected readonly title = signal('todo-list');
